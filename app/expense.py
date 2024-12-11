1710d0721ba07279492123139d67b56e11bf9283
miksuk28 <miksuk28@outlook.com>
2024-12-11 21:23:47 +0100

    added DuplicateItem expception

--- app/expense.py
+++ app/expense.py
@@ -1,5 +1,6 @@
 from db_manager import DatabaseConnection
 from db_operations import Expenses  as sql_e
+import sqlite3
 
 ### CUSTOM EXCEPTIONS - MOVE TO SEPARATE FILE IN THE FUTURE ###
 class UserNotFound(Exception):
@@ -7,7 +8,7 @@ class UserNotFound(Exception):
 
     Attributes:
         user_id - user id which was not found
-        suername - username which was not found
+        username - username which was not found
     """
 
     def __init__(self, id=None, username=None, message="User cannot be found in database"):
@@ -16,6 +17,21 @@ class UserNotFound(Exception):
         self.message =  message
         super().__init__(self.message)
 
+
+class DuplicateItem(Exception):
+    """Raised when an item with the same name for the user already exists
+    
+    Attributes:
+        user_id -   Affected user ID
+        name    -   Name of the conflicting item
+        expense_id - Affected expense ID
+    """
+
+    def __init__(self, user_id=None, name=None, expense_id=None,message="Item already added on this expense for the respective user"):
+        self.user_id =      user_id,
+        self.name    =      name,
+        self.expense_id =   expense_id,
+        self.message =      message
 ### END CUSTOM EXCEPTIONS ###
 
 
@@ -47,14 +63,23 @@ class Expense(DatabaseConnection):
     def add_item(self, username, name, price):
         user_id = self._get_user_id(username)
         item_id = self.genid()
-        with self.cur() as cur:
-            cur.execute(sql_e.ADD_ITEM, {
-                "id":            item_id,
-                "expenseId":     self.id,
-                "costBearerId":  user_id,
-                "name":          name,
-                "price":         price
-            })
+        try:
+            with self.cur() as cur:
+                cur.execute(sql_e.ADD_ITEM, {
+                    "id":            item_id,
+                    "expenseId":     self.id,
+                    "costBearerId":  user_id,
+                    "name":          name,
+                    "price":         price
+                })
+        
+        except sqlite3.IntegrityError:
+            # Raised if user already has an item with
+            # the same name for this expense_id
+            raise DuplicateItem(
+                user_id=user_id,
+                name=name
+            )
         
         return item_id
 
